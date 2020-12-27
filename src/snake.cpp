50a0a0026c6217281e7800eda042e2c9aa637ec5
Paweł Arnista <26674182+Backss4@users.noreply.github.com>
2020-12-28 00:48:05 +0100

    Screens and event handling concept

--- src/snake.cpp
+++ src/snake.cpp
@@ -1,6 +1,83 @@
 #include "snake.h"
 
-CSnake::CSnake(CRect r, char _c /*=' '*/):
-  CFramedWindow(r, _c)
-{
+/*
+ * Painting screens
+ */ 
+
+void CSnake::paint() {
+  CFramedWindow::paint();
+  paintLevelBar();
+  switch (state) {
+  // home screen state - first state after running program
+  case home_screen:
+    paintHomeScreen();
+    break;
+  // pause state
+  case game_paused:
+    paintPauseScreen();
+    break;
+  case in_game:
+    break;
+  // end game state
+  case end_game:
+    break;
+  }
 }
+
+void CSnake::paintLevelBar() {
+  gotoyx(geom.topleft.y - 1, geom.topleft.x);
+  printl("|LEVEL: %d|", level + 1);
+}
+
+void CSnake::paintHelpScreen() {
+  gotoyx(geom.topleft.y + 4, geom.topleft.x + 3);
+  printl("h - help");
+  gotoyx(geom.topleft.y + 5, geom.topleft.x + 3);
+  printl("p - toggle pause/play mode");
+  gotoyx(geom.topleft.y + 6, geom.topleft.x + 3);
+  printl("r - restart game");
+  gotoyx(geom.topleft.y + 7, geom.topleft.x + 3);
+  printl("arrows/WASD - move snake (in play mode)");
+  gotoyx(geom.topleft.y + 8, geom.topleft.x + 17);
+  printl("arrows - move window (in pause mode)");
+}
+
+void CSnake::paintHomeScreen() { 
+	paintHelpScreen(); 
+	gotoyx(geom.topleft.y + geom.size.y / 2,
+         geom.topleft.x + geom.size.x / 2 - 11);
+    printl("PRESS 'S' TO START GAME");
+}
+
+void CSnake::paintPauseScreen() {
+  gotoyx(geom.topleft.y + geom.size.y / 2,
+         geom.topleft.x + geom.size.x / 2 - 5);
+  printl("GAME PAUSED");
+}
+
+void CSnake::paintEndGameScreen() {
+  gotoyx(geom.topleft.y + 1, geom.topleft.x + 1);
+  printl("GAME OVER");
+  gotoyx(geom.topleft.y + 2, geom.topleft.x + 1);
+  printl("SCORE: ", score);
+}
+
+/*
+ * Events handling
+ */
+
+bool CSnake::handleEvent(int key) {
+  switch (state) {
+  case home_screen:
+    return handleHomeScreenEvent(key);
+  case game_paused:
+    return CFramedWindow::handleEvent(key);
+  case in_game:
+    return handleGameEvent(key);
+  }
+  return false;
+}
+
+bool CSnake::handleGameEvent(int key) { return false; }
+
+bool CSnake::handleHomeScreenEvent(int key) { return false; }
--- src/snake.h
+++ src/snake.h
@@ -2,12 +2,44 @@
 #define __SNAKE_H__
 
 #include "winsys.h"
+#include "cpoint.h"
+#include "screen.h"
 
 class CSnake:public CFramedWindow
 {
+	enum _states {
+		home_screen = 0,
+		game_paused,
+		in_game,
+		end_game,
+	};
+	enum _direction {
+		stop = 0,
+		left,
+		right,
+		up,
+		down,
+	};
+	
+	CPoint foodPos;
+	size_t score = 0;
+	size_t level = 0;
+	_states oldState = home_screen;
+	_states state = home_screen;
+	
+	//zarzadzanie stanem gry
+	bool handleGameEvent(int key);
+	bool handleHomeScreenEvent(int key);
+	
+	void paintLevelBar();
+	void paintHomeScreen();
+	void paintHelpScreen();
+	void paintPauseScreen();
+	void paintEndGameScreen();
 public:
-  CSnake(CRect r, char _c = ' ');
-
+  CSnake(CRect r, char _c = ' ') : CFramedWindow(r, _c) {};
+  void paint();
+  bool handleEvent(int key);
 };
 
 #endif
